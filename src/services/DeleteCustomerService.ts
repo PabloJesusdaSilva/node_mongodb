5c94d1f7576387654381b95e330825735ef7bdc4
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2024-01-31 05:57:59 -0300

    :feat: creating customer delete service and controller

---
+++ src/services/DeleteCustomerService.ts
@@ -0,0 +1,33 @@
+import prismaClient from '../prisma';
+
+interface DeleteCustomerProps {
+  id: string;
+}
+
+class DeleteCustomerService {
+  async execute({ id }: DeleteCustomerProps) {
+    if(!id) {
+      throw new Error('Invalid id!');
+    }
+
+    const findCustomer = await prismaClient.customer.findFirst({
+      where: {
+        id: id
+      }
+    })
+
+    if(!findCustomer) {
+      throw new Error('Invalid customer!');
+    }
+
+    await prismaClient.customer.delete({
+      where: {
+        id: findCustomer.id
+      }  
+    })
+
+    return { message: 'success' };
+  }
+}
+
+export { DeleteCustomerService }
